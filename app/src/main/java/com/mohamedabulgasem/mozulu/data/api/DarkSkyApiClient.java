cbb56447629b046f97d9b507696ebeea219fb1c4
Mohamed Abulgasem <algiriany93@gmail.com>
2019-04-23 00:45:22 +0200

    Created a convenience service method

--- app/src/main/java/com/mohamedabulgasem/mozulu/data/api/DarkSkyApiClient.java
+++ app/src/main/java/com/mohamedabulgasem/mozulu/data/api/DarkSkyApiClient.java
@@ -8,27 +8,34 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import static com.mohamedabulgasem.mozulu.utils.AppConstants.DARK_SKY_API_BASE_URL;
 
 /**
- * This is boundary class for interacting with the Events REST API.
+ * This is boundary class for interacting with the Dark Sky REST API.
  *
  * Created by Mohamed Abulgasem on 2019/04/16.
  */
 public final class DarkSkyApiClient {
 
     private static final String TAG = DarkSkyApiClient.class.getSimpleName();
-    private static Retrofit retrofit = null;
+    private static DarkSkyApiService service = null;
 
     /**
-     * @return Retrofit
+     * Private default constructor to ensure appropriate usage of this class
      */
-    public static Retrofit getRetrofit() {
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
+    private DarkSkyApiClient() {}
+
+    /**
+     * @return DarkSkyApiService
+     */
+    public static DarkSkyApiService getService() {
+        if (service == null) {
+            service = new Retrofit
+                    .Builder()
                     .baseUrl(DARK_SKY_API_BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-            Log.d(TAG, "Dark Sky API Client Initialised");
+                    .build()
+                    .create(DarkSkyApiService.class);
+            Log.d(TAG, "Dark Sky API Service Initialised");
         }
-        return retrofit;
+        return service;
     }
 
 }

bb5acf8eb2deaa07ee7bd04b143df7877beb86a3
Mohamed Abulgasem <algiriany93@gmail.com>
2019-04-23 01:04:06 +0200

    Added error code enum to represent possible errors that might occur

---
+++ app/src/main/java/com/mohamedabulgasem/mozulu/utils/ErrorCode.java
@@ -0,0 +1,10 @@
+package com.mohamedabulgasem.mozulu.utils;
+
+/**
+ * Created by Mohamed Abulgasem on 2019/04/22.
+ */
+public enum ErrorCode {
+    NO_CONNECTIVITY,
+    LOCATION_PERMISSION_DENIED,
+    FORECAST_REQUEST_FAILURE,
+}
